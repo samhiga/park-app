11ef29412178d249a07b82cdf8f8719efe9049f2
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-06-06 11:38:50 -0700

    created parking rental model

---
+++ server/models/ParkingRental.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const ParkingRentalSchema = new Schema({
+  owner: {
+    type: Schema.Types.ObjectID,
+    ref: "User",
+    required: true,
+  },
+  rentee: {
+    type: Schema.Types.ObjectID,
+    ref: "User",
+    required: true,
+  },
+  dateBookedStart: {
+    type: Date,
+    required: true,
+  },
+  dateBookedEnd: {
+    type: Date,
+    required: true,
+  },
+  pricePaid: {
+    type: Number,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+    required: true,
+  }
+  
+});
+
+const ParkingRental = model("ParkingRental", ParkingRentalSchema);
+
+module.exports = ParkingSpot;
