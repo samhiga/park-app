3d11a982953167470f3e0e06efaddb74b8a8d11f
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-06-06 15:37:52 -0700

    added seeds data

---
+++ server/seeders/seed.js
@@ -0,0 +1,34 @@
+const db = require('../config/connection');
+const {  User, ParkingSpot, ParkingRental } = require('../models');
+const userSeeds = require('./userSeeds.json');
+const parkingSpotSeeds = require('./parkingSpotSeeds.json');
+const parkingRentalSeeds = require('./parkingRentalSeeds.json');
+
+db.once('open', async () => {
+  try {
+    await ParkingRental.deleteMany({});
+    await ParkingSpot.deleteMany({});
+    await User.deleteMany({});
+
+    await ParkingSpot.create(parkingSpotSeeds);
+    await User.create(userSeeds);
+
+    for (let i = 0; i < parkingRentalSeeds.length; i++) {
+      const { _id, owner } = await ParkingRental.create(parkingRentalSeeds[i]);
+      const parkingSpot = await ParkingSpot.findOneAndUpdate(
+        { owner: owner },
+        {
+          $addToSet: {
+            rentals: _id,
+          },
+        }
+      );
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log('all done!');
+  process.exit(0);
+});
