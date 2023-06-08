1fc933c8da8114c7f114b8936c83553aa3409027
seantamturk <128218653+seantamturk@users.noreply.github.com>
2023-06-07 19:23:02 -0700

    Update mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,5 +26,47 @@ export const ADD_USER = gql`
   }
 `;
 
-export const ADD_PARKING_SPOT = gql`
-  mutation
+import { gql } from '@apollo/client';
+
+export const CREATE_PARKING_SPOT = gql`
+  mutation CreateParkingSpot(
+    $name: String!
+    $owner: String!
+    $streetAddress: String!
+    $zipcode: String!
+    $price: String!
+    $dateStart: Date!
+    $dateEnd: Date!
+  ) {
+    createParkingSpot(
+      name: $name
+      owner: $owner
+      streetAddress: $streetAddress
+      zipcode: $zipcode
+      price: $price
+      dateStart: $dateStart
+      dateEnd: $dateEnd
+    ) {
+      _id
+      name
+      owner {
+        _id
+        username
+      }
+      streetAddress
+      zipcode
+      price
+      active
+      dateStart
+      dateEnd
+      sunday
+      monday
+      tuesday
+      wednesday
+      thursday
+      friday
+      saturday
+    }
+  }
+`;
+
