45fb1173363561763396f26d07817d0468ea9d51
seantamturk <128218653+seantamturk@users.noreply.github.com>
2023-06-07 20:17:21 -0700

    fixed queries syntax, finished mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,21 +1,8 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-      token
-      user{
-        _id
-        username
-        email
-      }
-    }
-  }
-`;
-
-export const ADD_USER = gql`
-  mutation addUser ($username: String!, $email: String!, $password: String!) {
-    addUser (username: $username, email: $email, password: $password) {
       token
       user {
         _id
@@ -26,9 +13,18 @@ export const ADD_USER = gql`
   }
 `;
 
-export const UPDATE_USER = gql`
-  mutation updateUser (
-
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
 
 export const CREATE_PARKING_SPOT = gql`
   mutation CreateParkingSpot(
@@ -74,42 +70,42 @@ export const CREATE_PARKING_SPOT = gql`
 
 export const UPDATE_PARKING_SPOT = gql`
   mutation UpdateParkingSpot(
-    $_id: ID!,
-    $name: String,
-    $description: String,
-    $owner: ID,
-    $streetAddress: String,
-    $zipcode: String,
-    $price: Int,
-    $active: Boolean,
-    $dateStart: String,
-    $dateEnd: String,
-    $sunday: Boolean,
-    $monday: Boolean,
-    $tuesday: Boolean,
-    $wednesday: Boolean,
-    $thursday: Boolean,
-    $friday: Boolean,
+    $_id: ID!
+    $name: String
+    $description: String
+    $owner: ID
+    $streetAddress: String
+    $zipcode: String
+    $price: Int
+    $active: Boolean
+    $dateStart: String
+    $dateEnd: String
+    $sunday: Boolean
+    $monday: Boolean
+    $tuesday: Boolean
+    $wednesday: Boolean
+    $thursday: Boolean
+    $friday: Boolean
     $saturday: Boolean
   ) {
     updateParkingSpot(
-      _id: $_id,
+      _id: $_id
       input: {
-        name: $name,
-        description: $description,
-        owner: $owner,
-        streetAddress: $streetAddress,
-        zipcode: $zipcode,
-        price: $price,
-        active: $active,
-        dateStart: $dateStart,
-        dateEnd: $dateEnd,
-        sunday: $sunday,
-        monday: $monday,
-        tuesday: $tuesday,
-        wednesday: $wednesday,
-        thursday: $thursday,
-        friday: $friday,
+        name: $name
+        description: $description
+        owner: $owner
+        streetAddress: $streetAddress
+        zipcode: $zipcode
+        price: $price
+        active: $active
+        dateStart: $dateStart
+        dateEnd: $dateEnd
+        sunday: $sunday
+        monday: $monday
+        tuesday: $tuesday
+        wednesday: $wednesday
+        thursday: $thursday
+        friday: $friday
         saturday: $saturday
       }
     ) {
@@ -135,6 +131,3 @@ export const UPDATE_PARKING_SPOT = gql`
     }
   }
 `;
-
-
-
