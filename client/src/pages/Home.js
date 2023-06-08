d0acf3413c3cfbf99cff0ebbb042397883f341ce
Daniel Lam <Dann.lam@gmail.com>
2023-06-07 20:31:49 -0700

    added stuff :)

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,33 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import ParkingSpotCard from "../components/ParkingSpotCard";
-import { useQuery } from "@apollo/client";
-import { QUERY_PARKING_SPOTS } from "../utils/queries";
+import ParkingSpotList from "../components/ParkingSpotCard";
+// import { Link } from "react-router-dom";
+// import ParkingSpotCard from "../components/ParkingSpotCard";
+// import { useQuery } from "@apollo/client";
+// import { QUERY_PARKING_SPOTS } from "../utils/queries";
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_PARKING_SPOTS);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  const parkingSpotData = data?.parkingSpots || [];
 
   return (
     <div>
       <div className="container">
-        <div className="row">
-          {parkingSpotData.map((parkingSpot) => (
-            <div className="col-md-4" key={parkingSpot.id}>
-              <Link
-                to={`/parkingSpot/${parkingSpot.id}`}
-                style={{ textDecoration: "none" }}
-              >
-                <ParkingSpotCard parkingSpot={parkingSpot} />
-              </Link>
-            </div>
-          ))}
-        </div>
+        <ParkingSpotList />
       </div>
     </div>
   );
---
+++ client/src/utils/unused_reducers.js
@@ -0,0 +1,20 @@
+import { useReducer } from "react";
+import {
+  ADD_PARKING_SPOT,
+  UPDATE_PARKING_SPOT,
+  REMOVE_PARKING_SPOT,
+  ADD_USER,
+} from "./actions";
+
+// The reducer is a function that accepts the current state and an action. It returns a new state based on that action.
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD_USER:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+  }
+
+};
