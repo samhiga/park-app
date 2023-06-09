c77ed9a0f5ebe522a501b42ac74af8029209fe96
Daniel Lam <Dann.lam@gmail.com>
2023-06-09 16:36:41 -0700

    The first sin

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const { ApolloServer } = require("apollo-server-express");
-// const path = require("path");
+const path = require("path");
 
 // const { authMiddleware } = require('./utils/auth');
 
@@ -25,9 +25,9 @@ app.use(express.json());
 
 // app.use("/images", express.static(path.join(__dirname, "../client/images")));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../client/build/index.html"));
