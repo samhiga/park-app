8a1858a118d8f4e57b3093f37d7fd98e3ecd9b2b
samhiga <samhiga18@gmail.com>
2023-06-10 12:07:56 -0700

    Added date picker and working price calculator

--- client/src/pages/SpotDetails.js
+++ client/src/pages/SpotDetails.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { QUERY_SINGLE_PARKING_SPOT } from "../utils/queries";
 
 const SpotDetails = () => {
@@ -9,8 +11,11 @@ const SpotDetails = () => {
     variables: { id_inserter: spotId },
   });
 
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
   // const { loading, data } = useQuery(QUERY_PARKING_SPOTS);
-  //I want to KNOW if mty query single parking spot is correct.
+  //I want to KNOW if my query single parking spot is correct.
 
   if (loading) {
     return <div>Loading...</div>;
@@ -25,6 +30,26 @@ const SpotDetails = () => {
   if (!spot.owner) {
     spot.owner.username = "Username not found!";
   }
+
+  // function to set min end date to whatever user selects for start date
+  const handleStartDateChange = (date) => {
+    setStartDate(date);
+    // Update the minimum end date based on the selected start date
+    setEndDate((prevEndDate) => {
+      if (date > prevEndDate) {
+        return date;
+      }
+      return prevEndDate;
+    });
+  };
+
+  const calculatePrice = () => {
+    const start = startDate.getTime();
+    const end = endDate.getTime();
+    const days = (end - start) / (1000 * 3600 * 24);
+    return spot.price * days;
+  };
+
   return (
     <div>
       <div className="container">
@@ -39,7 +64,26 @@ const SpotDetails = () => {
         <p>Date Start: {spot.dateStart}</p>
         <p>Date End: {spot.dateEnd}</p>
         {/* <p>Schedule: {getScheduleString(spot)}</p> */}
-        <button type="button" class="btn btn-secondary">Rent Me</button>
+        <p>Pick Start Date</p>
+        <DatePicker
+          label="Pick Start Date"
+          name="startDate"
+          selected={startDate}
+          onChange={handleStartDateChange}
+          value={startDate}
+          required
+        />
+        <p>Pick End Date</p>
+        <DatePicker
+          name="endDate"
+          label="Pick End Date"
+          selected={endDate}
+          onChange={(date) => setEndDate(date)}
+          value={endDate}
+          minDate={startDate}
+          required
+        />
+        <p>Total Price: ${calculatePrice()}</p>
       </div>
     </div>
   );
